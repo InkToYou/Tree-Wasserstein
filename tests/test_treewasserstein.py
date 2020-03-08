330592dfec25347db56dcc25e6ca532991c42afb
cojiro <43770589+InkToYou@users.noreply.github.com>
2020-03-09 08:14:24 +0900

    Add tests

---
+++ tests/test_treewasserstein.py
@@ -0,0 +1,28 @@
+import pytest
+import numpy as np
+import ot
+import networkx as nx
+import tw
+
+
+class TestTreeWasserstein(object):
+  def test_tree_wasserstein(self):
+    for i in range(100):
+      num_node = np.random.randint(10, 200)
+      G = nx.generators.random_tree(num_node)
+      edges = [(fr, to, 1)
+               for (fr, to) in list(G.edges())]
+      first_prob = np.random.rand(num_node)
+      first_prob = first_prob / first_prob.sum()
+      second_prob = np.random.rand(num_node)
+      second_prob = second_prob / second_prob.sum()
+      twd = tw.tw(
+          first_prob=first_prob, second_prob=second_prob,
+          edges=edges)
+
+      adj_dict = dict(nx.all_pairs_shortest_path_length(G))
+      metric = np.array([[adj_dict[i][j]
+                          for i in range(num_node)] for j in range(num_node)])
+      ans = ot.lp.emd2(first_prob, second_prob, metric)
+
+      assert np.allclose(twd, [ans]), f"TW : {twd}, WD : {ans}"
